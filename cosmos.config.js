31c6199baa8a587da60d1bc7c6d03e3c2a8e0ebd
ɐʇɯon3ɹ <contact.atmoner@gmail.com>
2024-09-29 19:29:02 +0200

    Add files via upload

---
+++ cosmos.config.js
@@ -0,0 +1,133 @@
+export default [
+  {
+    chainId: 'bitcanna-1',
+    name: 'BitCanna',
+    desc: 'BitCanna Pay Instantaneous worldwide payments, effortlessly easy and private',
+    apiURL: 'https://lcd.bitcanna.io',
+    rpcURL: 'https://rpc.bitcanna.io',
+    coinLookup: {
+      viewDenom: 'BCNA',
+      chainDenom: 'ubcna',
+      addressPrefix: 'bcna',
+      icon: `https://raw.githubusercontent.com/cosmos/chain-registry/master/bitcanna/images/bcna.svg`
+    },
+    coingeckoId: 'bitcanna',
+    avatars: 'bitcanna',
+    color: '#0EA674',
+    colorChart: ['#f72047', '#0EA674', '#1feaea']
+  },
+  {
+    chainId: 'chihuahua-1',
+    name: 'Chihuahua',
+    desc: 'Chihuahua is the first interoperable meme coin with a POS blockchain, making it one of the most utilized chains in the Cosmos ecosystem',
+    apiURL: 'https://api.chihuahua.wtf',
+    rpcURL: 'https://rpc.chihuahua.wtf',
+    coinLookup: {
+      viewDenom: 'HUAHUA',
+      chainDenom: 'uhuahua',
+      addressPrefix: 'chihuahua',
+      icon: `https://avatars.githubusercontent.com/u/94940349?v=4`
+    },
+    coingeckoId: 'chihuahua-token',
+    avatars: 'chihuahua',
+    color: '#F0A841',
+    colorChart: ['#f72047', '#F0A841', '#1feaea']
+  },
+  {
+    chainId: 'comdex-1',
+    name: 'Comdex',
+    desc: 'Comdex is a decentralized synthetics protocol in the Cosmos Ecosystem.',
+    apiURL: 'https://rest.comdex.one',
+    rpcURL: 'https://rpc.comdex.one',
+    coinLookup: {
+      viewDenom: 'CMDX',
+      chainDenom: 'ucmdx',
+      addressPrefix: 'comdex',
+      icon: `https://raw.githubusercontent.com/cosmos/chain-registry/master/gitopia/images/lore.svg`
+    },
+    coingeckoId: 'comdex',
+    avatars: 'comdex',
+    color: '#F79AA3',
+    colorChart: ['#f72047', '#F79AA3', '#1feaea']
+  },
+  /*{
+      chainId: 'cosmoshub-4',
+      name: 'Cosmoshub',
+      desc: 'The Cosmos Hub is the first of thousands of interconnected blockchains that will eventually comprise the Cosmos Network.',
+      apiURL: 'https://lcd-cosmoshub.blockapsis.com',
+      rpcURL: 'https://cosmoshub.validator.network',
+      coinLookup: {
+        viewDenom: 'ATOM',
+        chainDenom: 'uatom',
+        addressPrefix: 'cosmos',
+        icon: `https://raw.githubusercontent.com/osmosis-labs/assetlists/main/images/atom.svg`,
+      }, 
+      coingeckoId: 'cosmos'
+    },  
+    {
+      chainId: 'akashnet-2',
+      name: 'Akash',
+      desc: 'Akash Network is a Distributed Peer-to-Peer Marketplace for Cloud Compute',
+      apiURL: 'https://rest-akash.ecostake.com',
+      rpcURL: 'https://rpc.akash.forbole.com',
+      coinLookup: {
+        viewDenom: 'AKT',
+        chainDenom: 'uakt',
+        addressPrefix: 'akash',
+        icon: `https://raw.githubusercontent.com/osmosis-labs/assetlists/main/images/akt.svg`,
+      },    
+      coingeckoId: 'akash-network'
+    }, */
+
+  {
+    chainId: 'osmosis-1',
+    name: 'Osmosis',
+    desc: 'Osmosis is a decentralized peer-to-peer blockchain that people can use to create liquidity and trade IBC enabled tokens.',
+    apiURL: 'https://lcd-osmosis.blockapsis.com',
+    rpcURL: 'https://osmosis.validator.network',
+    coinLookup: {
+      viewDenom: 'OSMO',
+      chainDenom: 'uosmo',
+      addressPrefix: 'osmo',
+      icon: `https://raw.githubusercontent.com/cosmos/chain-registry/master/osmosis/images/osmo.svg`
+    },
+    coingeckoId: 'osmosis',
+    avatars: 'osmosis',
+    color: '#A53BAA',
+    colorChart: ['#f72047', '#A53BAA', '#1feaea']
+  },
+  {
+    chainId: 'bitsong-2b',
+    name: 'BitSong',
+    desc: 'A Blockchain Ecosystem to Empower the Music Industry. Artists, fans, distributors and labels.',
+    apiURL: 'https://lcd-bitsong.itastakers.com',
+    rpcURL: 'https://rpc-bitsong.itastakers.com',
+    coinLookup: {
+      viewDenom: 'BTSG',
+      chainDenom: 'ubtsg',
+      addressPrefix: 'bitsong',
+      icon: `https://raw.githubusercontent.com/cosmos/chain-registry/master/bitsong/images/btsg.svg`
+    },
+    coingeckoId: 'bitsong',
+    avatars: 'bitsong',
+    color: '#FF005C',
+    colorChart: ['#f72047', '#FF005C', '#1feaea']
+  },
+  {
+    chainId: 'juno-1',
+    name: 'Juno',
+    desc: 'Juno is a decentralized effort to provide a sister hub to the Cosmos Hub.',
+    apiURL: 'https://lcd-juno.itastakers.com',
+    rpcURL: 'https://rpc-juno.itastakers.com',
+    coinLookup: {
+      viewDenom: 'JUNO',
+      chainDenom: 'ujuno',
+      addressPrefix: 'juno',
+      icon: `https://raw.githubusercontent.com/cosmos/chain-registry/master/juno/images/juno.svg`
+    },
+    coingeckoId: 'juno-network',
+    avatars: 'juno',
+    color: '#312526',
+    colorChart: ['#f72047', '#312526', '#1feaea']
+  }
+]
